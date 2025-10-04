318534896a1a8532c299e3fd61fcf56997260f92
Mwasdaym <johnchege2159@gmail.com>
2025-10-04 17:12:42 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+"name": "CHEGE TECH",
+"version": "1.0.0",
+"description": "CHEGE TECH",
+"main": "server.js",
+"scripts": {
+"start": "node server.js"
+},
+"dependencies": {
+"axios": "^1.4.0",
+"body-parser": "^1.20.2",
+"ejs": "^3.1.9",
+"express": "^4.18.2",
+"fs-extra": "^11.1.1",
+"nanoid": "^4.0.2"
+}
+}
